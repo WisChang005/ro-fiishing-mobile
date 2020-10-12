1f38cc436e0f1ce382d93a857e55c736f702b170
Wis Chang <wistw.chang@gmail.com>
2020-10-12 23:05:43 +0800

    feat: update script

--- ro_fishing.py
+++ ro_fishing.py
@@ -1,55 +1,41 @@
+import time
 import logging
 
 import pyautogui
 
 
-def get_fishing_button_position():
-    position = pyautogui.locateOnScreen("fishing_btn.png")
-    if position:
-        return get_picture_location(*list(position))
-    return None
-
-
-def wait_fishing_btn_available():
-    position = None
-    for t in range(2):
-        position = pyautogui.locateOnScreen("fishing_available.png")
-        if position:
-            position = get_picture_location(*list(position))
+def wait_for_color_changed(x, y):
+    for _ in range(20):
+        logging.info("Position: x=%s, y=%s", x, y)
+        im = pyautogui.screenshot()
+        r, g, b = im.getpixel((x, y))
+        logging.info("r=%s, g=%s, b=%s", r, g, b)
+        if is_the_color_available(r, g, b):
+            logging.info("The color is available [%s]", (r, g, b))
             break
-        logging.info("Wait for fishing available...[%s]", t)
-    return position
+        time.sleep(0.3)
 
 
-def position_checker(p):
-    if not p:
-        raise ValueError("Position not found")
-    logging.info("Position: %s found", p)
-
-
-def get_picture_location(x, y, w, h):
-    c_x, c_y = (x + (w/2.0)), (y + h/2.0)
-    return c_x, c_y
+def is_the_color_available(r, g, b):
+    r_th = 203
+    g_th = 235
+    b_th = 152
+    available_th = 3
+    if (r_th - r) <= available_th and (g_th - g) <= available_th and (b_th - b) <= available_th:
+        return True
+    return False
 
 
 def main():
     while True:
-        fish_btn = get_fishing_button_position()
-        if fish_btn:
-            pyautogui.moveTo(*fish_btn)
-            pyautogui.click(*fish_btn)
-        else:
-            logging.info("Fishing button not found")
-            continue
+        fish_btn = (1539, 747)
+        pyautogui.moveTo(*fish_btn)
+        pyautogui.click(*fish_btn)
+        wait_for_color_changed(*fish_btn)
 
-        fish_ava_p = wait_fishing_btn_available()
-        if fish_ava_p:
-            logging.info("Real deceted Fishing!!!!!")
-            pyautogui.moveTo(*fish_ava_p)
-            pyautogui.click(*fish_ava_p)
-        else:
-            logging.info("Trying to fishing...")
-            pyautogui.click(*fish_btn)
+        logging.info("Trying to pull fishing rod...")
+        pyautogui.click(*fish_btn)
+        time.sleep(4)
 
 
 if __name__ == "__main__":
